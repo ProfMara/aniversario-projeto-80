b32c001908c2c0dcf88869ce12bf34e1be071b91
ProfMara <94469709+ProfMara@users.noreply.github.com>
2022-11-03 21:14:07 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,35 @@
+
+//esse comando cria uma lista com os convidados
+
+function adicionar()
+{
+    //o valor que a pessoa digitou é guardado na variável convidado 
+  
+    //depois, disso, ele é adicionado na matriz
+
+    //e então, a matriz é organizada por ordem alfabética
+   
+
+    //esse comando limpa a caixa de texto
+   
+    //esses comandos exibe a lista de nomes
+    
+}
+
+function pesquisar(){
+    //esse comando guarda na variável o que a pessoa digitou para pesquisar
+    
+    //a variável quantidade guarda o número de vezes que o nome foi encontrado
+    
+    
+    
+        //esse comando compara o que a pessoa digitou com o nome da lista
+       
+            //quando são iguais, a quantidade aumenta
+           
+
+
+    //esse comando exibe a quantidade
+   
+}
+
